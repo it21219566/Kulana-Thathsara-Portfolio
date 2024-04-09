f2f949789eb14696c660e3515c31f3d50fa2e912
IT21219566 <it21219566@my.sliit.lk>
2024-04-09 19:56:24 +0530

    navbar modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,14 @@ import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
 } from "react-icons/ai";
-
 import { CgFileDocument } from "react-icons/cg";
+import { IoIosContact } from "react-icons/io";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -98,7 +97,7 @@ function NavBar() {
                 to="/resume"
                 onClick={() => updateExpanded(false)}
               >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Contact
+                <IoIosContact style={{ marginBottom: "2px" }} /> Contact
               </Nav.Link>
             </Nav.Item>
 
