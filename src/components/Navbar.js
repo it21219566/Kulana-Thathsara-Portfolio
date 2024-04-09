9499922dddec76e8df7f9b3e55b0e9f9ba072535
IT21219566 <it21219566@my.sliit.lk>
2024-04-09 17:26:48 +0530

    styles modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,17 +94,17 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
+                as={Link}
+                to="/resume"
+                onClick={() => updateExpanded(false)}
               >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
+                <CgFileDocument style={{ marginBottom: "2px" }} /> Contact
               </Nav.Link>
             </Nav.Item>
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/it21219566/Kulana-Thathsara-Portfolio.git"
                 target="_blank"
                 className="fork-btn-inner"
               >
